bf9e8cd3422a1daf12086f121b39fc024ccc682a
Tetsunosuke Koizumi <tkbagatelle@gmail.com>
2025-12-19 02:57:39 +0900

    feat: i18n設定(Next/Tailwind)の適用

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+    content: [
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                'serif-en': ['var(--font-playfair)', 'serif'],
+                'serif-ja': ['var(--font-zen-old-mincho)', 'serif'],
+                'sans-en': ['var(--font-inter)', 'sans-serif'],
+                'sans-ja': ['var(--font-noto-sans-jp)', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [],
+};
+export default config;
